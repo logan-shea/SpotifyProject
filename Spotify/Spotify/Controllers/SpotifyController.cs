d198e168d0944f99fd08947551382b25e6a2156a
Logan Shea <logan.a.shea97@ou.edu>
2019-04-23 18:04:10 -0500

    commiting everything

---
+++ Spotify/Spotify/Controllers/SpotifyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SpotifyAPI.Web; 
+using SpotifyAPI.Web.Auth; 
+using SpotifyAPI.Web.Enums; 
+using SpotifyAPI.Web.Models;
+
+namespace Spotify.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        // GET: Spotify
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private static SpotifyWebAPI _spotify;
+
+        public static void Main(String[] args)
+        {
+            _spotify = new SpotifyWebAPI()
+            {
+                UseAuth = false,
+            };
+                FullTrack track = _spotify.GetTrack("3Hvu1pq89D4R0lyPBoujSv");
+            Console.WriteLine(track.Name);
+        }
+    }
+}
