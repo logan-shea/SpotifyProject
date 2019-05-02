bc1c834cfc62a9f77139d283c486833168da7492
Logan Shea <logan.a.shea97@ou.edu>
2019-05-02 18:18:06 -0500

    trying to figure out how to get the api into a model now

--- Spotify/Spotify/Controllers/HomeController.cs
+++ Spotify/Spotify/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using SpotifyAPI.Web.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +13,20 @@ namespace Spotify.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string seachString)
         {
+            ClientCredentialsAuth client = new ClientCredentialsAuth();
+            client.ClientId = "559a2c8e37d946d782cc29bb373a63f0";
+            client.ClientSecret = "23915a1a8a5e406ea1df5bc8baec36e4";
+            Token token = client.DoAuth();
+            SpotifyWebAPI api = new SpotifyWebAPI();
+            api.AccessToken = token.AccessToken;
+            api.UseAuth = true;
+            api.UseAutoRetry = true;
+            api.TokenType = "Bearer";
+            SearchItem item = api.SearchItems("Sublime", SearchType.Artist);
+            var music = from FullArtist in item.Artists.ToString()
+                        select FullArtist;
             return View();
         }
 
