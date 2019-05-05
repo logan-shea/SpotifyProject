962f91345081dfdb6257a424da6058a7a6a54c31
Logan Shea <logan.a.shea97@ou.edu>
2019-05-04 19:53:04 -0500

    I give up for now, to be continued this summer

---
+++ Spotify/Spotify/Models/SpotifyArtist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spotify.Models
+{
+    public class SpotifyArtist
+    {
+        public string ID { get; set; }
+        public string NAME { get; set; }
+        public string URI { get; set; }
+    }
+}
